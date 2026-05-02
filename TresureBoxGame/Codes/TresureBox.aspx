f6b2388171970ac4b703f7edc5f26e04a15806cb
Eda Çalışkan <edacaliskan4402@gmail.com>
2026-05-02 10:11:07 +0300

    Initial commit of Tresure Box Game

---
+++ TresureBoxGame/Codes/TresureBox.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TresureBox.aspx.cs" Inherits="TresureBoxGame.TresureBox" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Tresure Box</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Tresure Box Game</h1>
+            <p>There are 10 hidden boxes. Choose as many as you want and see the results.</p>
+            <h3>Good luck !!!</h3>
+            <asp:TextBox ID="TextBox1" runat="server" Width="212px"></asp:TextBox>
+
+            <asp:Button ID="Button1" runat="server" Text="Enter the numbers" OnClick="Button1_Click" />
+            <br />
+            <br />
+        </div>
+        <p>
+            <asp:Image ID="Image1" runat="server" Height="346px" style="margin-right: 58px" Width="556px" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ TresureBoxGame/Codes/TresureBox.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TresureBoxGame
+{
+    public partial class TresureBox : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int[] box = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                box[i] = rnd.Next(-1, 11);
+            }
+
+            string[] myArr = TextBox1.Text.Split(',');
+            int[] chosen = new int[10];   
+            int count = 0;               
+
+            for (int i = 0; i < 10; i++)
+            {
+                 // TextBox3.Text += box[i].ToString() + " ";
+
+                for (int j = 0; j < myArr.Length; j++)
+                {
+                    if (box[i] == int.Parse(myArr[j]))
+                    {
+                        chosen[count] = box[i];  
+                        count++;                
+                        break;                    
+                    }
+                }
+            }
+
+
+
+            int total = 0;
+            int Trap = 0;
+            int Empty = 0;
+            int max = 0;
+            for (int i = 0; i < count; i++)
+            {
+                total += chosen[i];
+                if (chosen[i] == -1)
+                {
+                    Trap++;
+                }
+                else if(chosen[i] == 0)
+                {
+                    Empty++;
+                }
+                if (max < chosen[i])
+                {
+                    max= chosen[i];
+                }
+            }
+            if (Trap > 2)
+            {
+                loss();
+            }
+            else if(Trap<2 && total>30)
+            {
+                win();
+            }
+            else
+            {
+                loss();
+            }
+
+        }
+
+        protected void loss()
+        {
+            Image1.ImageUrl = "https://cdn.pixabay.com/photo/2021/11/05/11/49/game-over-6771162_1280.jpg";
+        }
+        protected void win()
+        {
+            Image1.ImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp8pLTzsqwvuUcRfEhHkEuoG7ysbtRSIyZ_w&s";
+        }
+
+       
+    }
+}
